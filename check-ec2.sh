a0f24f32ec2d163d5b5a536746235b96f0a90dd7
micytao <aus.michael.yang@gmail.com>
2025-09-16 15:24:30 +1000

    adding bash scripts

---
+++ check-ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws ec2 describe-instance-status --instance-ids i-0a693468cdcd21f5c --include-all-instances
---
+++ stop-ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws ec2 stop-instances --instance-ids i-0a693468cdcd21f5c --region us-east-2
